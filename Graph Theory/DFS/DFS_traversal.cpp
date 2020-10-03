67855ff0623147f2563e193897743546875c3c26
Arpit Singh <arpit.cse.nits@gmail.com>
2020-10-03 14:50:17 +0530

    Create DFS_traversal.cpp
    The above algorithm is the implementation of the DFS traversal of the graph using a recursive approach.
    Sample Input:
    N=5 , E=4
    0 1
    0 2
    0 3
    2 4
    
    Output:
    0 1 2 4 3
    
    Explanation:
    0 is connected to 1 , 2 , 3
    1 is connected to 0
    2 is connected to 0 and 4
    3 is connected to 0
    4 is connected to 2
    so starting from 0 , dfs will be 0 1 2 4 3.

---
+++ Graph Theory/DFS/DFS_traversal.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+/* Function to do DFS of graph
+*  g[]: array of vectors to represent graph
+*  vis[]: array to keep track of visited vertex
+*/
+void dfs(int s, vector<int> g[], bool vis[])
+{
+
+    //Mark visited
+    vis[s]=true;
+    cout<<s<<" ";
+    for(auto u: g[s])
+    {
+        if(!vis[u]) dfs(u,g,vis);
+    }
+
+}
+
+//Driver Code Starts here.
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+
+        int N, E;
+        cin>>N>>E;
+
+        vector<int> g[N];
+        bool vis[N];
+
+        memset(vis, false, sizeof(vis));
+
+        for(int i=0;i<E;i++)
+        {
+            int u,v;
+            cin>>u>>v;
+            g[u].push_back(v);
+            g[v].push_back(u);
+        }
+
+        dfs(0,g,vis);
+
+        cout<<endl;
+
+    }
+} 

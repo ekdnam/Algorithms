80cea12cb83b06bf3312d39b59ac24b4e04596c7
avnishsingh516 <52257822+avnishsingh516@users.noreply.github.com>
2020-10-03 18:36:06 +0530

    Update DFS.py
    changes made to take input dynamically

--- Graph Theory/DFS/DFS.py
+++ Graph Theory/DFS/DFS.py
@@ -46,12 +46,12 @@ class Graph:
 # Create a graph given  
 # in the above diagram 
 g = Graph() 
-g.addEdge(0, 1) 
-g.addEdge(0, 2) 
-g.addEdge(1, 2) 
-g.addEdge(2, 0) 
-g.addEdge(2, 3) 
-g.addEdge(3, 3) 
+E = int(input("Enter the number of edges:"))
+
+for i in range(0,E):
+  x, y = input("Enter a edges value: ").split()
+  g.addEdge(x, y) 
+
   
 print("Following is DFS from (starting from vertex 2)") 
 g.DFS(2) 

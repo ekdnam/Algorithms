6896d6e242ec128aa9d09f7f8f7bb3f9592c54f9
Clement <clementjoe99@gmail.com>
2020-10-03 18:47:14 +0530

    added newline and spaces

--- Searching/Linear Search/linearSearch.c
+++ Searching/Linear Search/linearSearch.c
@@ -1,30 +1,22 @@
 #include <stdio.h> 
-  
+
 int search(int arr[], int n, int x) 
 { 
-    int i; 
-    for (i = 0; i < n; i++) 
-        if (arr[i] == x) 
-            return i; 
-    return -1; 
+	int i; 
+	for (i = 0; i < n; i++) 
+		if (arr[i] == x) 
+			return i; 
+	return -1; 
 } 
-  
+
 int main(void) 
 { 
-    printf("Enter size of array");
-    int n,x,i;
-    scanf("%d",&n);
-    int arr[n];
-    printf("Enter elements of array");
-    for(i=0;i<n;i++)
-    {
-    scanf("%d",&arr[i]);
-    } 
-    printf("Enter Element u want to search.");
-    scanf("%d",&x);
-    int result = search(arr, n, x); 
-    (result == -1) ? printf("Element is not present in array") 
-                   : printf("Element is present at index %d", 
-                            result); 
-    return 0; 
+	int arr[] = { 2, 3, 4, 10, 40 }; 
+	int x = 10; 
+	int n = sizeof(arr) / sizeof(arr[0]); 
+	int result = search(arr, n, x); 
+	(result == -1) ? printf("Element is not present in array") 
+				: printf("Element is present at index %d", 
+							result); 
+	return 0; 
 } 
--- Sorting/Quicksort/quicksort.c
+++ Sorting/Quicksort/quicksort.c
@@ -1,29 +1,41 @@
 #include<stdio.h>
 void quicksort(int number[25],int first,int last){
+
    int i, j, pivot, temp;
 
    if(first<last){
+
       pivot=first;
+      
       i=first;
+      
       j=last;
 
-      while(i<j){
-         while(number[i]<=number[pivot]&&i<last)
+      while( i < j ){
+      
+         while( number[i] <= number[pivot] && i<last )
             i++;
-         while(number[j]>number[pivot])
+         while( number[j] > number[pivot] )
             j--;
-         if(i<j){
+         if( i < j ){
+
             temp=number[i];
+            
             number[i]=number[j];
+            
             number[j]=temp;
          }
       }
 
-      temp=number[pivot];
-      number[pivot]=number[j];
-      number[j]=temp;
-      quicksort(number,first,j-1);
-      quicksort(number,j+1,last);
+      temp = number[pivot];
+
+      number[pivot] = number[j];
+      
+      number[j] = temp;
+      
+      quicksort(number, first, j-1);
+      
+      quicksort(number, j+1, last);
 
    }
 }
@@ -32,17 +44,20 @@ int main(){
    int i, count, number[25];
 
    printf("Enter the number of elements u want to enter");
+   
    scanf("%d",&count);
 
    printf("Enter %d elements: ", count);
-   for(i=0;i<count;i++)
-      scanf("%d",&number[i]);
+   
+   for( i=0; i<count; i++)
+      scanf("%d", &number[i]);
 
-   quicksort(number,0,count-1);
+   quicksort( number, 0, count-1);
 
    printf("Order of Sorted elements: ");
-   for(i=0;i<count;i++)
-      printf(" %d",number[i]);
+   
+   for( i=0; i<count; i++)
+      printf(" %d", number[i]);
 
    return 0;
 }

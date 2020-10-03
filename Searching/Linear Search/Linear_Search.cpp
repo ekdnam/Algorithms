0b5e2cc62a1238b909aa040f8957f9d791ba2b98
Aditya Mandke <40426312+ekdnam@users.noreply.github.com>
2020-10-03 21:58:20 +0530

    prettify

--- Searching/Linear Search/Linear_Search.cpp
+++ Searching/Linear Search/Linear_Search.cpp
@@ -1,7 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
- int n;
- int linear_search(int x,int arr[])
+
+int n;
+ 
+int linear_search(int x,int arr[])
  {
      for(int i=0;i<n;i++)
      {
@@ -10,18 +12,24 @@ using namespace std;
      }
      return 0;
  }
+
 int main()
 {
-  cin>>n;
+  cin >> n;
   int arr[n];
-  for(int i=0;i<n;i++)
+  for(int i = 0 ; i<n ; i++)
   {
-      cin>>arr[i];
+      cin >> arr[i];
   }
-    int element;cin>>element;
-    if(linear_search(element,arr))
-        cout<<element<<" is present in the array";
-    else
-        cout<<element<<" is not present in the array";
+  int element;
+  cin>>element;
+ 
+  if(linear_search(element,arr)){
+     cout<<element<<" is present in the array";
+  }
+  else{
+    cout<<element<<" is not present in the array";
+  }
+ 
     return 0;
 }

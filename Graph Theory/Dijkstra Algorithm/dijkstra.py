a3c7b8f62b5ca16c1d184f79ce39fc86cdeff0d4
avnishsingh516 <52257822+avnishsingh516@users.noreply.github.com>
2020-10-03 18:24:41 +0530

    Update dijkstra.py
    make the program able to run on dynamic input

--- Graph Theory/Dijkstra Algorithm/dijkstra.py
+++ Graph Theory/Dijkstra Algorithm/dijkstra.py
@@ -67,17 +67,18 @@ class Graph():
 		self.printSolution(dist) 
 
 # Driver program 
-g = Graph(9) 
-g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0], 
-		[4, 0, 8, 0, 0, 0, 0, 11, 0], 
-		[0, 8, 0, 7, 0, 4, 0, 0, 2], 
-		[0, 0, 7, 0, 9, 14, 0, 0, 0], 
-		[0, 0, 0, 9, 0, 10, 0, 0, 0], 
-		[0, 0, 4, 14, 10, 0, 2, 0, 0], 
-		[0, 0, 0, 0, 0, 2, 0, 1, 6], 
-		[8, 11, 0, 0, 0, 0, 1, 0, 7], 
-		[0, 0, 2, 0, 0, 0, 6, 7, 0] 
-		]; 
+v = int(input("Enter the no : "))
+g = Graph(v) 
+
+g.graph = []
+
+# For user input 
+print("Enter the entries row wise:")
+for i in range(v):         
+    a =[] 
+    for j in range(v):     
+         a.append(int(input())) 
+    graph.append(a)
 
 g.dijkstra(0); 
 

58e70e5c5d003e35a781a491bc06a9a1cd1063c4
Saumya Mishra <saumya@Saumyas-MacBook-Air.local>
2020-10-03 15:22:38 +0530

    Topological sorting using DFS algorithm

---
+++ Graph Theory/TopologicalSort.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+
+void DFS(vector<int> a[], stack<int> &s,vector<int> &visited, int i){
+    visited[i] = true;
+    for(x : a[i]){
+        if(!visited[x]){
+        DFS(a,s,visited,x);
+        s.push(x);
+    }}
+    
+}
+// V is number of vertices and adj is adjacency list
+vector<int> topologicalSort(int V, vector<int> adj[]) {
+    vector<int> ts;
+    vector<int> visited(V,false);
+    stack<int> s;
+    for(int i=0;i<V;i++){
+        if(visited[i]==false){
+            DFS(adj,s,visited,i);
+            s.push(i);
+        }
+    }
+    while(s.empty()==false){
+        int u = s.top();
+        ts.push_back(u);
+        s.pop();
+    }
+    return ts;
+}
+
+int main()
+{
+int n,e;
+int u,v;
+cin>>n>>e;
+vector<int> adj_list[n];
+for(int i=0;i<e;i++){
+cin>>u>>v;
+adj_list[u].push_back(v);
+}
+vector<int> result = topologicalSort(n, adj_list);
+for(int i=0;i<n;i++){
+cout<<result[i]<<" ";
+}
+
+}
+
